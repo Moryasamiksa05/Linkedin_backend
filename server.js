aeab0ce1302daae4f9d6e3f4fffff25549331749
SamikshaMorya <moryasamiksha52@gmail.com>
2025-06-26 11:55:26 +0530

    updated

--- server.js
+++ server.js
@@ -12,16 +12,19 @@ import connectionRoutes from "./routes/connection.route.js";
 
 import { connectDB } from "./lib/db.js";
 
+// Load environment variables
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
+// CLIENT_URL is loaded
+console.log("Allowed Frontend URL:", process.env.CLIENT_URL);
 
 const allowedOrigins = [
 	"http://localhost:5173",
-	process.env.CLIENT_URL, 
+	process.env.CLIENT_URL,
 ];
 
 app.use(
@@ -30,7 +33,7 @@ app.use(
 			if (!origin || allowedOrigins.includes(origin)) {
 				callback(null, true);
 			} else {
-				callback(new Error("Not allowed by CORS"));
+				callback(new Error(" Not allowed by CORS: " + origin));
 			}
 		},
 		credentials: true,
@@ -40,14 +43,14 @@ app.use(
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 
-
+// API routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/notifications", notificationRoutes);
 app.use("/api/v1/connections", connectionRoutes);
 
-
+//  Serve frontend in production
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
@@ -57,6 +60,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+	console.log(` Server running on port ${PORT}`);
 	connectDB();
 });
